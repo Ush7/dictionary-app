5572cfe94baacfb66188c3c4fa3234021da42550
Ush7 <ush.shilp@gmail.com>
2024-05-09 13:01:05 -0400

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-
+  function handleResponse(response) {
+    console.log(response);
+  }
   function receiveWord(event) {
     event.preventDefault();
     alert(`Searching definition of word ${keyword}`);
+
+    let apiKey = "3eo0fetfbb61a575e45b64ff05342834";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleWord(event) {
     event.preventDefault();
