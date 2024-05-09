0540c66aee22d00750dfe74a4553e02e367c8ea1
Ush7 <ush.shilp@gmail.com>
2024-05-09 11:11:02 -0400

    Integrated bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import "bootstrap";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">Hello World !!!</header>
     </div>
   );
 }
-
-export default App;
